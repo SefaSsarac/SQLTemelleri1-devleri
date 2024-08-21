34d99e213ec59a88237add9c0f204edb4732fddf
Sefa Saraç <srcsfysf@gmail.com>
2024-08-21 04:09:50 +0300

    Add files via upload

---
+++ SQLTemelleri1Ödev1.sql
@@ -0,0 +1,13 @@
+SELECT title, description FROM film;  --1.PRATİK
+
+SELECT * FROM film
+WHERE length > 60 AND length < 75 ;   --2.PRATİK
+
+SELECT * FROM film
+WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);  --3. PRATİK 
+
+SELECT last_name FROM customer
+WHERE first_name = 'Mary';    --4.Pratik 
+
+SELECT * FROM film
+WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);  --4.PRATİK
